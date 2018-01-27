a4b99c1886d0b4ac37b943decf69fc3a18055888
Gregor <gregor@martynus.net>
2018-01-26 16:16:57 -0800

    feat: initial version

---
+++ bin/load.js
@@ -0,0 +1,55 @@
+#! /usr/bin/env node
+
+const {table} = require('table')
+
+const getServiceCenterProcessingTimes = require('..')
+
+const [centerId] = process.argv.slice(2)
+
+if (centerId) {
+  if (isNaN(parseInt(centerId, 10))) {
+    console.log(`${centerId} is not a valid USCIS center ID, must be a number`)
+    process.exit(1)
+  }
+
+  getServiceCenterProcessingTimes({id: centerId})
+
+  .then(logResult)
+
+  .catch(error => {
+    console.log(error)
+    process.exit(1)
+  })
+} else {
+  getServiceCenterProcessingTimes()
+
+  .then((result) => {
+    Object.keys(result).map(centerId => result[centerId]).forEach(logResult)
+  })
+
+  .catch(error => {
+    console.log(error.message)
+    process.exit(1)
+  })
+}
+
+function logResult (result) {
+  console.log(`${result.shortName} - ${result.longName} (${result.id})`)
+
+  Object.keys(result.processingTimes).forEach(formId => {
+    console.log(`\n${formId}\n`)
+
+    const forms = Object.keys(result.processingTimes[formId]).reduce((list, description) => {
+      list.push([description, result.processingTimes[formId][description]])
+      return list
+    }, [])
+
+    console.log(table(forms))
+  })
+
+  console.log(`\nLast updated: ${result.lastUpdated}\n`)
+}
+
+function logUsage () {
+  console.log()
+}
---
+++ index.js
@@ -0,0 +1,54 @@
+module.exports = getServiceCenterProcessingTimes
+
+const pRetry = require('p-retry')
+
+const getCenters = require('./lib/get-centers')
+const getProcessingTimes = require('./lib/get-processing-times')
+
+async function getServiceCenterProcessingTimes (options) {
+  const state = {
+    sessionId: null,
+    centerId: options && options.id,
+    timeout: 1000,
+    retries: 3
+  }
+  const centers = await getCenters(state)
+
+  if (state.centerId && !centers[state.centerId]) {
+    const allCenterIdsString = Object.keys(centers).map(id => {
+      return `${id}: ${centers[id].shortName} – ${centers[id].longName}`
+    }).join('\n')
+    throw new Error(`No center exists with id: ${state.centerId}. Must be one of\n${allCenterIdsString}`)
+  }
+
+  return Object.keys(centers).reduce(async (promise, centerId) => {
+    await promise
+
+    if (state.centerId && state.centerId !== parseInt(centerId, 10)) {
+      return Promise.resolve(centers)
+    }
+
+    const center = centers[centerId]
+
+    // delay to not trip over the USCIS server
+    const processingTimes = await pRetry(
+      getProcessingTimes.bind(null, state, centerId),
+      {
+        retries: state.retries,
+        minTimeout: state.timeout,
+        randomize: true
+      }
+    )
+    Object.assign(center, processingTimes)
+
+    return Promise.resolve(centers)
+  }, Promise.resolve())
+
+  .then(centers => {
+    if (state.centerId) {
+      return centers[state.centerId]
+    }
+
+    return centers
+  })
+}
